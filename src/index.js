4e103dc68fb74bc8f8e8ce24587c18039d859256
phantom-67 <swapnil67priyadarshi@gmail.com>
2024-05-12 01:29:36 +0530

    index edit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom'; // Import HashRouter
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter> {/* Wrap your App component with HashRouter */}
       <App />
-    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
