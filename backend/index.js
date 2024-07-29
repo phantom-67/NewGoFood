a5fbf50b726049602f1fd6bff10bc689444e75b9
phantom-67 <68739835+phantom-67@users.noreply.github.com>
2024-07-29 18:26:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,28 +13,16 @@ const app = express()
 const port = 4000
 const Mongodb = require("./db")
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// })
-
-const allowedOrigins = ["http://localhost:3000", "https://phantom-67.github.io"];
-
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
-});
+})
+
+const allowedOrigins = ["http://localhost:3000", "https://phantom-67.github.io"];
 
 Mongodb().then(() => {
   app.use(express.json());
